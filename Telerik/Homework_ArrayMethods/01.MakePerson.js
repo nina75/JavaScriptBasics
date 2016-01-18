7d8af7614b9859cea8a00f0209d081b7472a5342
nina75 <kozasbrada28>
2016-01-18 18:12:34 +0200

    Make person

---
+++ Telerik/Homework_ArrayMethods/01.MakePerson.js
@@ -0,0 +1,24 @@
+/* Write a function for creating persons.
+   Each person must have firstname, lastname, age and gender (true is female, false is male)
+   Generate an array with ten person with different names, ages and genders  */
+(function() {
+    var person = {},
+        persons = [];
+    function buildPerson(firstName, lastName, age, gender) {
+        return {
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            gender: gender
+        }
+    }
+
+    for (var i = 0; i < 10; i++) {
+        person = buildPerson('Minka' + i, 'Minkova' + i, (Math.random() * 100) | 0, i % 2 ? 'true' : 'false');
+        persons.push(person);
+    }
+
+    console.log(persons);
+})();
+
+
