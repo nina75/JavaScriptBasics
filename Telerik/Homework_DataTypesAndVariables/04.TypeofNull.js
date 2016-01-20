d9e2360287866446bf2c7aa495ac6abe97f2a1b2
nina75 <kozasbrada28>
2016-01-20 17:01:21 +0200

    Typeof null

---
+++ Telerik/Homework_DataTypesAndVariables/04.TypeofNull.js
@@ -0,0 +1,11 @@
+//Create null, undefined variables and try typeof on them.
+var a = null,
+    b = undefined,
+    c = [],
+    d = {},
+    e = '';
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
