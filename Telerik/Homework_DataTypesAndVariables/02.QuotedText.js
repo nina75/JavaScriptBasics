37554cf5270fe22bebff1a381c74920f39ab0929
nina75 <kozasbrada28>
2016-01-20 16:57:39 +0200

    Quoted text

---
+++ Telerik/Homework_DataTypesAndVariables/02.QuotedText.js
@@ -0,0 +1,9 @@
+//Create a string variable with quoted text in it.
+//For example: `'How you doin'?', Joey said'.
+
+var str = '"How you doin?", Joey said.',
+    str1 = "\"How you doin?\", Joey said.",
+    str2 = '\'How you doin?\', Joey said.';
+console.log(str);
+console.log(str1);
+console.log(str2);
