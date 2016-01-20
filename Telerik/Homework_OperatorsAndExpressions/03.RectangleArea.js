0ff8e1f369b5f18b7462de1673616e921af74c67
nina75 <kozasbrada28>
2016-01-20 15:58:16 +0200

    Rectangle area

---
+++ Telerik/Homework_OperatorsAndExpressions/03.RectangleArea.js
@@ -0,0 +1,8 @@
+//Write an expression that calculates rectangle’s area by given width and height.
+function calcArea(width, height) {
+    return width * height;
+}
+
+console.log(calcArea(3, 4));
+console.log(calcArea(2.5, 3));
+console.log(calcArea(5, 5));
