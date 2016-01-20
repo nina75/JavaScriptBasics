5710ec65aa2084e065243f3cd6bcbc26a84746df
nina75 <kozasbrada28>
2016-01-20 14:13:13 +0200

    Lexicographically smallest property

---
+++ Telerik/Homework _Loops/04.LexicographicallySmallest.js
@@ -0,0 +1,20 @@
+//Write a script that finds the lexicographically smallest and largest property in document, window and navigator objects.
+function findLexSmallestProperty(obj) {
+    var smallestProp = 'z';
+    for(var prop in obj) {
+        if(compareStr(prop, smallestProp) === -1) {
+            smallestProp = prop;
+        }
+    }
+
+    return smallestProp;
+}
+function compareStr(str1, str2) {
+    return str1.localeCompare(str2);
+}
+
+console.log(findLexSmallestProperty(document)); //activeElement
+console.log(findLexSmallestProperty(window)); //addEventListener
+console.log(findLexSmallestProperty(navigator)); //appCodeName
+
+
