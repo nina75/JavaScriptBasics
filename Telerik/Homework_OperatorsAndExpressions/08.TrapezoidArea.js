e681a79fdd93411b1e8b2f8090bf7e2dfb015f07
nina75 <kozasbrada28>
2016-01-20 16:27:55 +0200

    Calculates trapezoid's area

---
+++ Telerik/Homework_OperatorsAndExpressions/08.TrapezoidArea.js
@@ -0,0 +1,11 @@
+//Write an expression that calculates trapezoid's area by given sides a and b and height h.
+
+function calcArea(a, b, h) {
+    return (a + b) * h / 2;
+}
+
+console.log(calcArea(5, 7, 12));
+console.log(calcArea(2, 1, 33));
+console.log(calcArea(8.5, 4.3, 2.7));
+console.log(calcArea(100, 200, 300));
+console.log(calcArea(0.222, 0.333, 0.555));
