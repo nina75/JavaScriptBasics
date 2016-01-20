c72204f0301f5499e1278802b24ee1ffe7dd644f
nina75 <kozasbrada28>
2016-01-20 02:01:08 +0200

    Print numbers

---
+++ Telerik/Homework _Loops/01.Numbers.js
@@ -0,0 +1,8 @@
+//Write a script that prints all the numbers from 1 to N.
+function printNumbers(from, to) {
+    for (var i = from; i <= to; i++) {
+        console.log(i);
+    }
+}
+
+printNumbers(1, 10);
