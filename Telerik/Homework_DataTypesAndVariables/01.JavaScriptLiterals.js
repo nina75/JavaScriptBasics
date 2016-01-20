5b11de620e0a0b2a2a80a693e207bfd8fc1aad3c
nina75 <kozasbrada28>
2016-01-20 16:53:30 +0200

    JavaScript types

---
+++ Telerik/Homework_DataTypesAndVariables/01.JavaScriptLiterals.js
@@ -0,0 +1,22 @@
+//Assign all the possible JavaScript literals to different variables.
+var str = 'Pesho', //string
+    num = 5,
+    num1 = 5.123,
+    num2 = Infinity,
+    arr = [1, 2, 3],
+    obj = {name: 'Pesho', age: 19},
+    fun = function (a) { return a; },
+    u = undefined,
+    isValid = true,
+    n = null;
+
+console.log(typeof str);//string
+console.log(typeof num);//number
+console.log(typeof num1);//number
+console.log(typeof num2);//number
+console.log(typeof arr);//object
+console.log(typeof obj); //object
+console.log(typeof fun); //function
+console.log(typeof u); //undefined
+console.log(typeof isValid); //boolean
+console.log(typeof n); //object
