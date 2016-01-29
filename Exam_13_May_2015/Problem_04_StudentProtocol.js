ea754e96edcb0a51513b594c639c10cf764400b9
nina75 <kozasbrada28>
2016-01-29 02:08:37 +0200

    Renaming some variables

--- Exam_13_May_2015/Problem_04_StudentProtocol.js
+++ Exam_13_May_2015/Problem_04_StudentProtocol.js
@@ -2,6 +2,7 @@ function solve(arr) {
     var regex = /(.+?)\s*-\s*(.+?)\s*:\s*(\d+)/,
         students = {},
         sortedStudents = {};
+
     arr.forEach(function(el) {
         var tokens = el.match(regex),
             studentName = tokens[1].trim(),
@@ -39,53 +40,6 @@ function solve(arr) {
     });
     console.log(JSON.stringify(sortedStudents));
 }
-function solve(arr) {
-    var regex = /(.+)\s*-\s*(.+)\s*:\s*(\d+)/,
-        result = {},
-        sortedResult = {};
-    arr.forEach(function(el){
-        var data = el.match(regex);
-        var studentName = data[1].trim(),
-            exam = data[2].trim(),
-            score = +data[3],
-            newStudent = {"name":studentName,"result": score,"makeUpExams": 0};
-        if(score >= 0 && score <=400) {
-            if(!result[exam]) {
-                result[exam] = [newStudent];
-            } else {
-                var student = result[exam].filter(function(s){return s.name == studentName})[0];
-                if(student) {
-                    student.result = Math.max(student.result, score);
-                    student.makeUpExams++;
-                } else {
-                    result[exam].push(newStudent);
-                }
-            }
-        }
-    });
-    Object.keys(result).forEach(function(exam) {
-       sortedResult[exam] = result[exam].sort(function(x, y) {
-           if(x.result === y.result) {
-               if(x.makeUpExams === y.makeUpExams) {
-                   return x.name.localeCompare(y.name);
-               }
-               return x.makeUpExams - y.makeUpExams;
-           }
-           return y.result - x.result;
-       });
-    });
-    console.log(JSON.stringify(sortedResult));
-}
-//solve([
-//    'Mila Kunis - C# : 200',
-//    'Mila Kunis - Java : 100',
-//    'Mila Kunis                          -PHP : 350',
-//    'Mila Kunis-HTML & CSS:400',
-//    'Jessica Clement - C# : 200',
-//    'Jessica Clement - Java : 100',
-//    'Jessica Clement - Java : 100',
-//    'Jessica Clement-PHP:300'
-//]);
 
 solve([
     'Selena Gomez - HTML & CSS : 400',
